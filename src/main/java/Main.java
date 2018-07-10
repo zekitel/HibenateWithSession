5ca8ef14942b6f424e4e8326c03fa4d1ea6f61d7
zeki.tel <zeki.tel@intra.local>
2018-07-10 15:02:27 +0300

    postgresql kullanıldı

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,21 +2,31 @@ import Entity.Employee;
 import org.hibernate.Session;
 
 
-
+import java.io.Serializable;
 import java.util.Date;
+import java.util.List;
 
 public class Main {
+
+
     public static void main(String[] args) {
         Employee employee=new Employee();
 
+        employee.setId(4);
         employee.setName("asdasd");
-        employee.setRole("asdasdas");
+        employee.setRole("AHMET");
         employee.setInsertTime(new Date());
 
 
         Session session= HibernateUtil.getSessionAnnotationFactory().getCurrentSession();
         session.beginTransaction();
-        session.save(employee);
+        List labels = session.createQuery("from employee").list();
+        //session.save(employee);
+        //Employee temp= (Employee) session.get(Employee.class,22);
+        //System.out.println(temp.getName()+"  "+temp.getRole());
+        //session.saveOrUpdate(employee);
+        System.out.println(labels);
+
 
 
         session.getTransaction().commit();
